b32b257abad5444b8143cc3727ca614878da380e
Konrad Walas <kwalas314@gmail.com>
2021-01-01 22:35:49 +0100

    Add simple database

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 5000;
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
 
 express()
@@ -15,4 +16,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
+  .get('/db', async (req, res) => {
+    try{
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM czlonek');
+      const results = {'results': (result) ? result.rows : null};
+      res.render('db', results );
+      client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
