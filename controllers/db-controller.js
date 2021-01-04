028f954a685400283d4d1b9de166777ba12e1eb7
Konrad Walas <kwalas314@gmail.com>
2021-01-05 00:09:28 +0100

    Add showing all tables

--- controllers/db-controller.js
+++ controllers/db-controller.js
@@ -1,10 +1,19 @@
 const pool = require('../db/database-connection');
 
 class DbController {
-  selectCzlonek = async (req, res) => {
+  selectAll = async (req, res) => {
     try {
-      const result = await pool.query('SELECT * FROM aktualne_wypozyczenia');
-      res.render('pages/db', { results: (result) ? result : null, isLogged: req.session.loggedin });
+      const allTableNames = await pool.query("select table_name from information_schema.tables where table_schema='public' and table_type='BASE TABLE'");
+      const resultTables = [];
+
+      for (const table of allTableNames.rows){
+        const results = await pool.query(`SELECT * FROM ${table.table_name}`);
+        if(results){
+          resultTables.push({tableName: table.table_name, results});
+        }
+      }
+
+      res.render('pages/db', { resultTables, isLogged: req.session.loggedin });
     }
     catch (err) {
       console.error(err);
--- routers/db-router.js
+++ routers/db-router.js
@@ -6,5 +6,6 @@ const middlewares = require('../middlewares/middleware-functions');
 const router = Router();
 
 router.get('/', middlewares.checkSignIn, controller.selectCzlonek);
+router.get('/all', controller.selectAll);
 
 module.exports = router;
