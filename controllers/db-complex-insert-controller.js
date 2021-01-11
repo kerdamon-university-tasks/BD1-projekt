77731b678816bd1f54325d0a17bb49662fed7f52
Konrad Walas <kwalas314@gmail.com>
2021-01-11 20:05:22 +0100

    Add working spotkanie_obecnosc inserting

--- controllers/db-complex-insert-controller.js
+++ controllers/db-complex-insert-controller.js
@@ -3,15 +3,89 @@ const { complexTableNames, complexFormNames, tableNamesWithAutonumeratedFirstId 
 
 class ComplexInsertController
 {
-  showForm_spotkanie_obecnosc = async (req, res) => {
+  insertInto_spotkanie_obecnosc = async (req, res) => {
     const tableName = req.params.tableName;
+    const tableData = req.body;
+
+    const client = await pool.connect();
+    try {
+      await client.query('BEGIN')
+
+      // get spotkanie_id
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      let spotkanie_id_QueryText; 
+      let spotkanie_id_values;
+      for (const elem in tableData) {
+        if(elem.includes('spotkanie')){
+          if(elem.includes('data')){
+            spotkanie_id_QueryText = `INSERT INTO spotkanie VALUES (default, $1) RETURNING spotkanie_id`;
+            spotkanie_id_values = [tableData[elem]];
+          }
+        }
+      }
+      const result = await client.query(spotkanie_id_QueryText, spotkanie_id_values)
+      let spotkanie_id = result.rows[0].spotkanie_id;
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // prepare obecnosc queries
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      let querries = [];
+      let obecnoscData = {}
+      for (const elem in tableData) {
+        // console.log(`${elem} -- ${tableData[elem]}`);
+  
+        if(elem.includes('obecnosc')){
+          let id = elem.substring(elem.length - 5);
+          
+          if(!(Object.keys(obecnoscData).includes(id))){
+            obecnoscData[id] = {obecny: false, oplacono_skladke: false};
+          }
+  
+          if(tableData[elem] === 'true'){
+            if(elem.includes('obecny')){
+              obecnoscData[id].obecny = true;
+            }
+            else if (elem.includes('oplacono_skladke')){
+              obecnoscData[id].oplacono_skladke = true;
+            }
+          }
+        }
+      }
+  
+      for (const key in obecnoscData) {
+        querries.push({query: `UPDATE obecnosc SET obecny=$1, oplacono_skladke=$2 WHERE spotkanie_id=$3 AND czlonek_id=$4`, values: [obecnoscData[key].obecny, obecnoscData[key].oplacono_skladke, spotkanie_id, parseInt(key)]})
+      }
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // console.log(querries);
+
+      querries.forEach(element => {
+        client.query(element.query, element.values);
+      });
+
+      await client.query('COMMIT')
+      res.redirect(`/db/allTables#spotkanie`);
+    } catch (e) {
+      await client.query('ROLLBACK')
+      throw e
+    } finally {
+      client.release()
+    }
+  }
+
+  showForm_spotkanie_obecnosc = async (req, res) => {
     const results_spotkanie = await pool.query(`SELECT * FROM spotkanie`);
     const results_obecnosc = await pool.query(`SELECT * FROM obecnosc`);
     const results_czlonek = await pool.query('SELECT * FROM czlonek');
 
     let results_spotkanie_fields = [];
     results_spotkanie.fields.forEach(field => {
-      results_spotkanie_fields.push({name: field.name, state: 'autonumerated'});
+      let state;
+      if(field.name === 'spotkanie_id')
+        state = 'autonumerated';
+      else
+        state = 'normal';
+      results_spotkanie_fields.push({name: field.name, state});
     });
 
     let results_obecnosc_fields = [];
@@ -32,7 +106,6 @@ class ComplexInsertController
       multipleInsert
     }
 
-    let excludeFirst = true;
     res.render('pages/db/insert-form/extended-insert-form', { isLogged: req.session.loggedin, tables});
   }
 
--- routers/db-insert-router.js
+++ routers/db-insert-router.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 
-const controller = require('../controllers/db-insert-controller');
+const simpleInsertController = require('../controllers/db-insert-controller');
+const complexInsertController = require('../controllers/db-complex-insert-controller');
 const middlewares = require('../middlewares/middleware-functions');
 
 
 const router = Router();
 
-router.get('/:tableName', controller.showInsertForm);
-router.post('/insertIntoTable/:tableName', controller.insertIntoTable);
-router.post('/complexInsertIntoTable/:tableName', controller.insertIntoTable);
+router.get('/:tableName', simpleInsertController.showInsertForm);
+router.post('/insertIntoTable/:tableName', simpleInsertController.insertIntoTable);
+router.post('/complexInsertIntoTable/spotkanie_obecnosc', complexInsertController.insertInto_spotkanie_obecnosc);
 
 // router.get('/:tableName', middlewares.checkSignIn, controller.showInsertForm);
 // router.post('/insertIntoTable/:tableName', middlewares.checkSignIn, controller.insertIntoTable);
