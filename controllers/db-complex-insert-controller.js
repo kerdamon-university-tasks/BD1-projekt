9d2c97224ad22f07cbe97565317bb1d6186aae97
Konrad Walas <kwalas314@gmail.com>
2021-01-12 18:26:46 +0100

    Add form to zarzad

--- controllers/db-complex-insert-controller.js
+++ controllers/db-complex-insert-controller.js
@@ -137,6 +137,70 @@ class ComplexInsertController
     }
   }
 
+  insertInto_zarzad_czlonek_zarzadu = async (req, res) => {
+    const tableName = req.params.tableName;
+    const tableData = req.body;
+
+    const client = await pool.connect();
+    try {
+      await client.query('BEGIN');
+
+      // get zarzad_id
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      let zarzad_id_QueryText; 
+      let zarzad_id_values = [];
+      for (const elem in tableData) {
+        if(elem.includes('zarzad-')){
+          if(elem.includes('data_od')){
+            zarzad_id_values.push(tableData[elem]);
+          }
+          if(elem.includes('data_do')){
+            zarzad_id_values.push(null);
+          }
+        }
+      }
+
+      zarzad_id_QueryText = `INSERT INTO zarzad VALUES (default, $1, $2) RETURNING zarzad_id`;
+
+      const result = await client.query(zarzad_id_QueryText, zarzad_id_values)
+      let zarzad_id = result.rows[0].zarzad_id;
+      // console.log(zarzad_id);
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      // prepare czlonek_zarzadu queries
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      let querries = [];
+      let czlonek_zarzaduData = {}
+      for (const elem in tableData) {
+  
+        if(elem.includes('czlonek_zarzadu')){
+          let id = elem.substring(elem.length - 5);
+  
+          if (tableData[elem] != ''){
+            czlonek_zarzaduData[id] = tableData[elem];
+          }
+        }
+      }
+  
+      for (const key in czlonek_zarzaduData) {
+        querries.push({query: `INSERT INTO czlonek_zarzadu VALUES ($1, $2, $3)`, values: [zarzad_id, parseInt(key), czlonek_zarzaduData[key]]});
+      }
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      for (const element of querries) {
+        await client.query(element.query, element.values);
+      }
+
+      await client.query('COMMIT')
+      res.redirect(`/db/allTables#zarzad`);
+    } catch (err) {
+      await client.query('ROLLBACK')
+      res.render('pages/db-error', { isLogged: req.session.loggedin, err });
+    } finally {
+      client.release()
+    }
+  }
+
   showForm_spotkanie_obecnosc = async (req, res) => {
     const results_spotkanie = await pool.query(`SELECT * FROM spotkanie`);
     const results_obecnosc = await pool.query(`SELECT * FROM obecnosc`);
@@ -210,7 +274,41 @@ class ComplexInsertController
   }
 
   showForm_zarzad_czlonek_zarzadu = async (req, res) => {
-    res.send("nie gotowe");
+    const results_zarzad = await pool.query(`SELECT * FROM zarzad`);
+    const results_czlonek_zarzadu = await pool.query(`SELECT * FROM czlonek_zarzadu`);
+    const results_czlonek = await pool.query('SELECT * FROM czlonek');
+
+    let zarzad_fields = [];
+    results_zarzad.fields.forEach(field => {
+      let state;
+      if(field.name === 'zarzad_id')
+        state = 'autonumerated';
+      else if (field.name === 'data_do')
+        state = 'null'
+      else
+        state = 'normal';
+        zarzad_fields.push({name: field.name, state});
+    });
+
+    let czlonek_zarzadu_fields = [];
+    results_czlonek_zarzadu.fields.forEach(field => {
+      czlonek_zarzadu_fields.push({name: field.name, type: field.dataTypeID, state: 'normal'});
+    });
+
+    let czlonek_zarzadu_values = [];
+    results_czlonek.rows.forEach(row => {
+      czlonek_zarzadu_values.push(row.czlonek_id);
+    });
+
+    let singleInsert = [{name: 'zarzad', fields: zarzad_fields}];
+    let multipleInsert = [{name: 'czlonek_zarzadu', commonFieldName: 'zarzad_id', secondPkField: {name: 'czlonek_id', values: czlonek_zarzadu_values}, fields: czlonek_zarzadu_fields}];
+
+    let tables = {
+      singleInsert,
+      multipleInsert
+    }
+
+    res.render('pages/db/insert-form/extended-insert-form', { isLogged: req.session.loggedin, tables});
   }
 }
 
--- routers/db-insert-router.js
+++ routers/db-insert-router.js
@@ -11,6 +11,6 @@ router.get('/:tableName', middlewares.checkSignIn, simpleInsertController.showIn
 router.post('/insertIntoTable/:tableName', middlewares.checkSignIn, simpleInsertController.insertIntoTable);
 router.post('/complexInsertIntoTable/spotkanie_obecnosc', middlewares.checkSignIn, complexInsertController.insertInto_spotkanie_obecnosc);
 router.post('/complexInsertIntoTable/wydarzenie_uczestnik', middlewares.checkSignIn, complexInsertController.insertInto_wydarzenie_uczestnik);
-
+router.post('/complexInsertIntoTable/zarzad_czlonek_zarzadu', middlewares.checkSignIn, complexInsertController.insertInto_zarzad_czlonek_zarzadu);
 
 module.exports = router;
